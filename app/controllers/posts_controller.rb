77e81db03c94243b218060a4b61134ea05da2149
Xin <Detective_boys@hotmil.com>
2019-01-19 18:26:11 +0800

    implement post#edit and permission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :find_post_and_check_permission, only: [:edit, :update]
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,7 +23,24 @@ class PostsController < ApplicationController
      end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Post Updated"
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You Have No Permission"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
